9d53446781394e67c6a63f1688008d36909691ff
Felipe Costa <lipecatornado@gmail.com>
2020-10-31 18:12:00 -0300

    criando informações pra tabela

---
+++ src/informacao.java
@@ -0,0 +1,22 @@
+package src;
+
+public class informacao {
+
+	private String codigo;
+	private String termo;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getTermo() {
+		return termo;
+	}
+	public void setTermo(String termo) {
+		this.termo = termo;
+	}
+	
+	
+}
